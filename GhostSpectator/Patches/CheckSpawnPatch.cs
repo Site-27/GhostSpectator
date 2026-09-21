708f8c39f2759af40c85ba6483e9da1d566e8c22
z5tm <onthebehalfofz5tm@gmail.com>
2026-09-21 04:15:18 -0500

    fix(check-spawn-patch): harmony patching, update methods

--- GhostSpectator/Patches/CheckSpawnPatch.cs
+++ GhostSpectator/Patches/CheckSpawnPatch.cs
@@ -1,28 +1,38 @@
-﻿using Achievements.Handlers;
-using HarmonyLib;
-using Respawning;
+﻿using InventorySystem.Items.Scp1509;
 
 namespace GhostSpectator.Patches;
 
+using HarmonyLib;
+using PlayerRoles.RoleAssign;
 
-
-
-    /// <summary>
-    /// Allows respawn wave to start when all spectators are ghosts.
-    /// </summary>
-    [HarmonyPatch(typeof(RespawnHandler), nameof(RespawnHandler.OnInitialize))]
-    public class CheckSpawnPatch
+/// <summary>
+/// Allows respawn wave to start when all spectators are ghosts.
+/// </summary>
+[HarmonyPatch(typeof(RoleAssigner), nameof(RoleAssigner.CheckPlayer))]
+public static class CheckSpawnPatch
+{
+    public static bool Prefix(ref bool __result, ReferenceHub hub)
     {
-        public static bool Prefix(RespawnHandler __instance, ReferenceHub ply, ref bool __result)
-        {
-            if (API.IsGhost(ply))
-            {
-                __result = true;
-                return false;
-            }
-
+        if (!API.IsGhost(hub))
             return true;
-        }
+        
+        __result = true;
+        return false;
     }
+}
 
-
+/// <summary>
+/// Allows ghosts to spawn under 1509 !.
+/// </summary>
+[HarmonyPatch(typeof(Scp1509RespawnEligibility), nameof(Scp1509RespawnEligibility.IsEligible))]
+public class CheckSpawn1509Patch
+{
+    public static bool Prefix(ref bool __result, ReferenceHub player)
+    {
+        if (!API.IsGhost(player))
+            return true;
+        
+        __result = true;
+        return false;
+    }
+}
